6749c308e1d9c7ecd065cd78781fde0a070db8a6
tamal <tamal@appscode.com>
2017-05-30 21:27:38 -0700

    Print flags on start

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	v "github.com/appscode/go/version"
+	logs "github.com/appscode/log/golog"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
 
 func main() {
+	defer logs.FlushLogs()
+
 	rootCmd := &cobra.Command{
 		Use:   "kubed",
 		Short: `Run Kubernetes Daemon for AppsCode`,
@@ -20,11 +24,15 @@ func main() {
 		},
 	}
 	rootCmd.PersistentFlags().AddGoFlagSet(flag.CommandLine)
+	// ref: https://github.com/kubernetes/kubernetes/issues/17162#issuecomment-225596212
+	flag.CommandLine.Parse([]string{})
+	logs.InitLogs()
 
 	rootCmd.AddCommand(NewCmdRun())
 	rootCmd.AddCommand(v.NewCmdVersion())
 
 	if err := rootCmd.Execute(); err != nil {
-		log.Fatal(err)
+		os.Exit(1)
 	}
+	os.Exit(0)
 }
--- run.go
+++ run.go
@@ -10,37 +10,37 @@ import (
 
 func NewCmdRun() *cobra.Command {
 	opt := pkg.Options{
+		APIEndpoint:           "https://api.appscode.com:3443",
 		APITokenPath:          "/var/run/secrets/appscode/api-token",
-		APIEndpoint:           "api.appscode.com:50077",
+		EnablePromMonitoring:  false,
 		InfluxSecretName:      "appscode-influx",
 		InfluxSecretNamespace: "kube-system",
-		EnablePromMonitoring:  false,
 	}
 	cmd := &cobra.Command{
 		Use:   "run",
 		Short: "Run daemon",
 		Run: func(cmd *cobra.Command, args []string) {
-			if opt.APIEndpoint == "" ||
-				opt.ClusterName == "" ||
-				opt.APITokenPath == "" {
+			if opt.APITokenPath == "" ||
+				opt.APIEndpoint == "" ||
+				opt.ClusterName == "" {
 				log.Fatalln("required flag not provided.")
 			}
-			log.Infoln("Starting Kubed Process...")
+			log.Infoln("Starting kubed...")
 			go pkg.Run(opt)
 
 			hold.Hold()
 		},
 	}
 
-	cmd.Flags().StringVar(&opt.APITokenPath, "api-token", opt.APITokenPath, "Endpoint of elasticsearch")
-	cmd.Flags().StringVar(&opt.Master, "master", opt.Master, "The address of the Kubernetes API server (overrides any value in kubeconfig)")
-	cmd.Flags().StringVar(&opt.KubeConfig, "kubeconfig", opt.KubeConfig, "Path to kubeconfig file with authorization information (the master location is set by the master flag).")
 	cmd.Flags().StringVar(&opt.APIEndpoint, "api-endpoint", opt.APIEndpoint, "appscode api server host:port")
+	cmd.Flags().StringVar(&opt.APITokenPath, "api-token", opt.APITokenPath, "File path for AppsCode api token.")
 	cmd.Flags().StringVar(&opt.ClusterName, "cluster-name", opt.ClusterName, "Name of Kubernetes cluster")
+	cmd.Flags().BoolVar(&opt.EnablePromMonitoring, "enable-prometheus-monitoring", opt.EnablePromMonitoring, "Enable Prometheus monitoring")
 	cmd.Flags().StringVar(&opt.ESEndpoint, "es-endpoint", opt.ESEndpoint, "Endpoint of elasticsearch")
 	cmd.Flags().StringVar(&opt.InfluxSecretName, "influx-secret", opt.InfluxSecretName, "Influxdb secret name")
 	cmd.Flags().StringVar(&opt.InfluxSecretNamespace, "influx-secret-namespace", opt.InfluxSecretNamespace, "Influxdb secret namespace")
-	cmd.Flags().BoolVar(&opt.EnablePromMonitoring, "enable-prometheus-monitoring", opt.EnablePromMonitoring, "Enable Prometheus monitoring")
+	cmd.Flags().StringVar(&opt.KubeConfig, "kubeconfig", opt.KubeConfig, "Path to kubeconfig file with authorization information (the master location is set by the master flag).")
+	cmd.Flags().StringVar(&opt.Master, "master", opt.Master, "The address of the Kubernetes API server (overrides any value in kubeconfig)")
 
 	return cmd
 }
