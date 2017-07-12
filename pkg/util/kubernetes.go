951644c264439e7761e021267850afa199775dd2
tamal <tamal@appscode.com>
2017-07-12 15:08:18 -0700

    added-all

---
+++ pkg/util/kubernetes.go
@@ -0,0 +1,22 @@
+package util
+
+import (
+	sapi "github.com/appscode/stash/api"
+	clientset "k8s.io/client-go/kubernetes"
+)
+
+func IsPreferredAPIResource(kubeClient clientset.Interface, groupVersion, kind string) bool {
+	if resourceList, err := kubeClient.Discovery().ServerPreferredResources(); err == nil {
+		for _, resources := range resourceList {
+			if resources.GroupVersion != groupVersion {
+				continue
+			}
+			for _, resource := range resources.APIResources {
+				if resources.GroupVersion == groupVersion && resource.Kind == kind {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
