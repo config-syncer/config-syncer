b4e722a741e80178634c0b9b0bc19bdd58a5e3a9
tamal <tamal@appscode.com>
2017-07-13 15:35:30 -0700

    added-all

--- pkg/operator/namespace.go
+++ pkg/operator/namespace.go
@@ -1,21 +1,28 @@
 package operator
 
 import (
+	acrt "github.com/appscode/go/runtime"
 	"github.com/appscode/kubed/pkg/configsync"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
 	apiv1 "k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/cache"
 )
 
 func (op *Operator) watchNamespaces() {
-	_, controller := cache.NewInformer(
-		cache.NewListWatchFromClient(
-			op.KubeClient.CoreV1().RESTClient(),
-			"namespaces",
-			metav1.NamespaceAll,
-			fields.Everything()),
+	defer acrt.HandleCrash()
+
+	lw := &cache.ListWatch{
+		ListFunc: func(opts metav1.ListOptions) (runtime.Object, error) {
+			return op.KubeClient.CoreV1().Namespaces().List(metav1.ListOptions{})
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			return op.KubeClient.CoreV1().Namespaces().Watch(metav1.ListOptions{})
+		},
+	}
+	_, ctrl := cache.NewInformer(lw,
 		&apiv1.Namespace{},
 		op.syncPeriod,
 		cache.ResourceEventHandlerFuncs{
@@ -25,5 +32,5 @@ func (op *Operator) watchNamespaces() {
 			},
 		},
 	)
-	go controller.Run(wait.NeverStop)
+	ctrl.Run(wait.NeverStop)
 }
--- pkg/operator/service.go
+++ pkg/operator/service.go
@@ -5,8 +5,9 @@ import (
 	"github.com/appscode/kubed/pkg/util"
 	"github.com/appscode/log"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
 	apiv1 "k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/cache"
 )
@@ -18,11 +19,14 @@ func (op *Operator) watchService() {
 	}
 
 	defer acrt.HandleCrash()
-	lw := cache.NewListWatchFromClient(
-		op.KubeClient.CoreV1().RESTClient(),
-		"services",
-		metav1.NamespaceAll,
-		fields.Everything())
+	lw := &cache.ListWatch{
+		ListFunc: func(opts metav1.ListOptions) (runtime.Object, error) {
+			return op.KubeClient.CoreV1().ConfigMaps(apiv1.NamespaceAll).List(metav1.ListOptions{})
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			return op.KubeClient.CoreV1().ConfigMaps(apiv1.NamespaceAll).Watch(metav1.ListOptions{})
+		},
+	}
 	_, controller := cache.NewInformer(lw,
 		&apiv1.Service{},
 		op.syncPeriod,
