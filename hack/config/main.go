0f2b3fa688ec8b4a213afe9b2d687efc64dfab16
tamal <tamal@appscode.com>
2017-07-13 19:12:56 -0700

    Generate configmap

--- hack/config/main.go
+++ hack/config/main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
-	"github.com/appscode/kubed/pkg/config"
-	"github.com/appscode/go/runtime"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"io/ioutil"
 	"time"
+
 	"github.com/appscode/go-notify/mailgun"
+	"github.com/appscode/go/runtime"
+	"github.com/appscode/kubed/pkg/config"
+	"github.com/appscode/log"
+	"github.com/ghodss/yaml"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
 )
 
 func main() {
-	cfg := config.ClusterConfig{
+	cfg := CreateClusterConfig()
+	cfg.Save(runtime.GOPath() + "/src/github.com/appscode/kubed/hack/config/kubed.yaml")
+
+	bytes, err := yaml.Marshal(cfg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	cfgmap := apiv1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind: "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "kubed",
+			Namespace: "default",
+		},
+		Data: map[string]string{
+			"config": string(bytes),
+		},
+	}
+	bytes, err = yaml.Marshal(cfgmap)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	p := runtime.GOPath() + "/src/github.com/appscode/kubed/hack/config/configmap.yaml"
+	ioutil.WriteFile(p, bytes, 0644)
+}
+
+func CreateClusterConfig() config.ClusterConfig {
+	return config.ClusterConfig{
 		TrashCan: &config.TrashCanSpec{
 			Path:         "/tmp/kubed",
 			TTL:          metav1.Duration{7 * 24 * time.Hour},
@@ -36,5 +69,4 @@ func main() {
 		},
 		NotifierSecretName: "",
 	}
-	cfg.Save(runtime.GOPath() + "/src/github.com/appscode/kubed/hack/config/kubed.yaml")
 }
