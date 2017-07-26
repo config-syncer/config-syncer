25c28c4f5bb356d98b3ae386851d7bdcef257237
tamal <tamal@appscode.com>
2017-07-25 22:13:25 -0700

    added-all

---
+++ hack/deploy/uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+kubectl delete deployment -l app=kubed -n kube-system
+kubectl delete service -l app=kubed -n kube-system
+kubectl delete secret -l app=kubed -n kube-system
+
+# Delete RBAC objects, if --rbac flag was used.
+kubectl delete serviceaccount -l app=kubed -n kube-system
+kubectl delete clusterrolebindings -l app=kubed -n kube-system
+kubectl delete clusterrole -l app=kubed -n kube-system
