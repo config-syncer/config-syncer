0e22ba6ae52b0fb8a3a54bc5a1eccf89454d26f3
Dipta Das <dipta670@gmail.com>
2017-12-19 16:58:08 +0600

    Fixed e2e tests (#157)

--- test/e2e/e2e.go
+++ test/e2e/e2e.go
@@ -1,13 +1,17 @@
 package e2e
 
 import (
+	"path/filepath"
 	"testing"
 	"time"
 
+	"github.com/appscode/kubed/pkg/cmds"
+	"github.com/appscode/kubed/pkg/operator"
 	"github.com/appscode/kubed/test/framework"
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
+	"k8s.io/client-go/util/homedir"
 )
 
 const TestTimeout = 1 * time.Hour
@@ -16,14 +20,14 @@ var (
 	root *framework.Framework
 )
 
-func TestE2ESuit(t *testing.T) {
+func RunE2ETestSuit(t *testing.T) {
 	RegisterFailHandler(Fail)
 	SetDefaultEventuallyTimeout(TestTimeout)
 
 	root = framework.New()
 
 	junitReporter := reporters.NewJUnitReporter("report.xml")
-	RunSpecsWithDefaultAndCustomReporters(t, "Kubed E2e Suite", []Reporter{junitReporter})
+	RunSpecsWithDefaultAndCustomReporters(t, "Kubed E2E Suite", []Reporter{junitReporter})
 }
 
 var _ = BeforeSuite(func() {
@@ -32,6 +36,20 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	err = root.EnsureCreatedCRDs()
 	Expect(err).NotTo(HaveOccurred())
+
+	// configure and run operator
+	opt := operator.Options{
+		KubeConfig:        filepath.Join(homedir.HomeDir(), ".kube/config"),
+		ConfigPath:        "config.yaml",
+		APIAddress:        ":8080",
+		WebAddress:        ":56790",
+		ScratchDir:        "/tmp/kubed",
+		OperatorNamespace: root.Namespace(),
+		ResyncPeriod:      5 * time.Minute,
+	}
+
+	By("Running kubed operator")
+	go cmds.Run(opt)
 })
 
 var _ = AfterSuite(func() {
