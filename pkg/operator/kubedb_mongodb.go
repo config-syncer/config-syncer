f1ff2739bc536bbce82503ae85bd9466a2229d97
tamal <tamal@appscode.com>
2018-01-05 14:45:45 -0800

    Add support for new DB types

---
+++ pkg/operator/kubedb_mongodb.go
@@ -0,0 +1,96 @@
+package operator
+
+import (
+	"errors"
+	"reflect"
+
+	"github.com/appscode/go/log"
+	"github.com/appscode/kutil/meta"
+	tapi "github.com/k8sdb/apimachinery/apis/kubedb/v1alpha1"
+	kutil "github.com/k8sdb/apimachinery/client/typed/kubedb/v1alpha1/util"
+	core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	rt "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/tools/cache"
+)
+
+// Blocks caller. Intended to be called as a Go routine.
+func (op *Operator) WatchMongoDBs() {
+	if !meta.IsPreferredAPIResource(op.KubeClient, tapi.SchemeGroupVersion.String(), tapi.ResourceKindMongoDB) {
+		log.Warningf("Skipping watching non-preferred GroupVersion:%s Kind:%s", tapi.SchemeGroupVersion.String(), tapi.ResourceKindMongoDB)
+		return
+	}
+
+	defer rt.HandleCrash()
+
+	lw := &cache.ListWatch{
+		ListFunc: func(opts metav1.ListOptions) (runtime.Object, error) {
+			return op.KubeDBClient.MongoDBs(core.NamespaceAll).List(metav1.ListOptions{})
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			return op.KubeDBClient.MongoDBs(core.NamespaceAll).Watch(metav1.ListOptions{})
+		},
+	}
+	_, ctrl := cache.NewInformer(lw,
+		&tapi.MongoDB{},
+		op.Opt.ResyncPeriod,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				if res, ok := obj.(*tapi.MongoDB); ok {
+					log.Infof("MongoDB %s@%s added", res.Name, res.Namespace)
+					kutil.AssignTypeKind(res)
+
+					if op.Config.APIServer.EnableSearchIndex {
+						if err := op.SearchIndex.HandleAdd(obj); err != nil {
+							log.Errorln(err)
+						}
+					}
+				}
+			},
+			DeleteFunc: func(obj interface{}) {
+				if res, ok := obj.(*tapi.MongoDB); ok {
+					log.Infof("MongoDB %s@%s deleted", res.Name, res.Namespace)
+					kutil.AssignTypeKind(res)
+
+					if op.Config.APIServer.EnableSearchIndex {
+						if err := op.SearchIndex.HandleDelete(obj); err != nil {
+							log.Errorln(err)
+						}
+					}
+					if op.TrashCan != nil {
+						op.TrashCan.Delete(res.TypeMeta, res.ObjectMeta, obj)
+					}
+				}
+			},
+			UpdateFunc: func(old, new interface{}) {
+				oldRes, ok := old.(*tapi.MongoDB)
+				if !ok {
+					log.Errorln(errors.New("invalid MongoDB object"))
+					return
+				}
+				newRes, ok := new.(*tapi.MongoDB)
+				if !ok {
+					log.Errorln(errors.New("invalid MongoDB object"))
+					return
+				}
+				kutil.AssignTypeKind(oldRes)
+				kutil.AssignTypeKind(newRes)
+
+				if op.Config.APIServer.EnableSearchIndex {
+					op.SearchIndex.HandleUpdate(old, new)
+				}
+				if op.TrashCan != nil && op.Config.RecycleBin.HandleUpdates {
+					if !reflect.DeepEqual(oldRes.Labels, newRes.Labels) ||
+						!reflect.DeepEqual(oldRes.Annotations, newRes.Annotations) ||
+						!reflect.DeepEqual(oldRes.Spec, newRes.Spec) {
+						op.TrashCan.Update(newRes.TypeMeta, newRes.ObjectMeta, old, new)
+					}
+				}
+			},
+		},
+	)
+	ctrl.Run(wait.NeverStop)
+}
