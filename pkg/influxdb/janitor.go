f1dfb8bb5e8d2037553865a6e1271d79aecfba0e
Tamal Saha <tamal@appscode.com>
2018-01-17 00:43:26 -0800

    Log influx janitor result (#185)

--- pkg/influxdb/janitor.go
+++ pkg/influxdb/janitor.go
@@ -6,6 +6,7 @@ import (
 	"net/url"
 	"time"
 
+	"github.com/appscode/go/log"
 	"github.com/appscode/kubed/pkg/config"
 	influxdb "github.com/influxdata/influxdb/client"
 )
@@ -30,10 +31,19 @@ func (j *Janitor) Cleanup() error {
 		return err
 	}
 
+	/*
+		// ref: https://docs.influxdata.com/influxdb/v1.4/query_language/schema_exploration/#example-2-run-a-show-retention-policies-query-without-the-on-clause
+		$ curl -G "http://localhost:8086/query?db=k8s&pretty=true" --data-urlencode "q=SHOW RETENTION POLICIES"
+	*/
 	query := influxdb.Query{
 		Command:  fmt.Sprintf("ALTER RETENTION POLICY default ON k8s DURATION %vs", int(math.Ceil(j.TTL.Seconds()))),
 		Database: "k8s",
 	}
 	_, err = client.Query(query)
+	if err != nil {
+		log.Warningf("failed to ALTER RETENTION POLICY for k8s database. Reason: %v", err)
+	} else {
+		log.Infoln("successfully ALTER-ed RETENTION POLICY for k8s database")
+	}
 	return err
 }
