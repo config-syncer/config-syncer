aff867a2f5bd0b02f3294a7e529e28372908e94e
Tamal Saha <tamal@appscode.com>
2018-03-31 10:09:19 -0700

    Delete internal clientset (#246)

--- apis/kubed/types.go
+++ apis/kubed/types.go
@@ -10,18 +10,18 @@ import (
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 type SearchResult struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
+	metav1.TypeMeta
+	metav1.ObjectMeta
 
-	Hits     []ResultEntry   `json:"hits,omitempty"`
-	Total    uint64          `json:"totalHits"`
-	MaxScore float64         `json:"maxScore"`
-	Took     metav1.Duration `json:"took"`
+	Hits     []ResultEntry
+	Total    uint64
+	MaxScore float64
+	Took     metav1.Duration
 }
 
 var _ runtime.Object = &SearchResult{}
 
 type ResultEntry struct {
-	Score  float64              `json:"score"`
-	Object runtime.RawExtension `json:"object,omitempty"`
+	Score  float64
+	Object runtime.RawExtension
 }
