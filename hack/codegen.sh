3e5fc28ae7ee31d016505569f4b71ec470332dd9
tamal <tamal@appscode.com>
2018-03-31 10:08:29 -0700

    Delete internal clientset

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -12,14 +12,35 @@ pushd $REPO_ROOT
 
 rm -rf "$REPO_ROOT"/apis/kubed/v1alpha1/*.generated.go
 
+# for EAS types
 docker run --rm -ti -u $(id -u):$(id -g) \
   -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
   -w "$DOCKER_REPO_ROOT" \
-  appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-internal-groups.sh all \
+  appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-internal-groups.sh "deepcopy,defaulter,conversion" \
   github.com/appscode/kubed/client \
   github.com/appscode/kubed/apis \
   github.com/appscode/kubed/apis \
   kubed:v1alpha1 \
   --go-header-file "$DOCKER_REPO_ROOT/hack/gengo/boilerplate.go.txt"
+
+# for both CRD and EAS types
+docker run --rm -ti -u $(id -u):$(id -g) \
+  -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
+  -w "$DOCKER_REPO_ROOT" \
+  appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-groups.sh all \
+  github.com/appscode/kubed/client \
+  github.com/appscode/kubed/apis \
+  kubed:v1alpha1 \
+  --go-header-file "$DOCKER_REPO_ROOT/hack/gengo/boilerplate.go.txt"
+
+# Generate openapi
+docker run --rm -ti -u $(id -u):$(id -g) \
+    -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
+    -w "$DOCKER_REPO_ROOT" \
+    appscode/gengo:release-1.9 openapi-gen \
+    --v 1 --logtostderr \
+    --go-header-file "hack/gengo/boilerplate.go.txt" \
+    --input-dirs "$PACKAGE_NAME/apis/kubed/v1alpha1" \
+    --output-package "$PACKAGE_NAME/apis/kubed/v1alpha1"
 
 popd
