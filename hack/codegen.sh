f1b74cb760f3e1708de2362e20eafda4d054bea4
Tamal Saha <tamal@appscode.com>
2018-05-03 17:17:15 -0700

    Update client-go to 7.0.0 (#261)

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -16,7 +16,7 @@ rm -rf "$REPO_ROOT"/apis/kubed/v1alpha1/*.generated.go
 docker run --rm -ti -u $(id -u):$(id -g) \
   -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
   -w "$DOCKER_REPO_ROOT" \
-  appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-internal-groups.sh "deepcopy,defaulter,conversion" \
+  appscode/gengo:release-1.10 "$DOCKER_CODEGEN_PKG"/generate-internal-groups.sh "deepcopy,defaulter,conversion" \
   github.com/appscode/kubed/client \
   github.com/appscode/kubed/apis \
   github.com/appscode/kubed/apis \
@@ -27,7 +27,7 @@ docker run --rm -ti -u $(id -u):$(id -g) \
 docker run --rm -ti -u $(id -u):$(id -g) \
   -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
   -w "$DOCKER_REPO_ROOT" \
-  appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-groups.sh all \
+  appscode/gengo:release-1.10 "$DOCKER_CODEGEN_PKG"/generate-groups.sh all \
   github.com/appscode/kubed/client \
   github.com/appscode/kubed/apis \
   kubed:v1alpha1 \
@@ -37,7 +37,7 @@ docker run --rm -ti -u $(id -u):$(id -g) \
 docker run --rm -ti -u $(id -u):$(id -g) \
     -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
     -w "$DOCKER_REPO_ROOT" \
-    appscode/gengo:release-1.9 openapi-gen \
+    appscode/gengo:release-1.10 openapi-gen \
     --v 1 --logtostderr \
     --go-header-file "hack/gengo/boilerplate.go.txt" \
     --input-dirs "$PACKAGE_NAME/apis/kubed/v1alpha1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/version,k8s.io/api/core/v1" \
