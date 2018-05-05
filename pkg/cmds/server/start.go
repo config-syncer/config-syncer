2c059bca09b6c32c2c93521f92ab5ad05ca5fa10
Tamal Saha <tamal@appscode.com>
2018-05-05 10:08:47 -0700

    Disable admission controllers for webhook server (#263)
    Since [1.10 release](https://github.com/kubernetes/apiserver/blob/release-1.10/pkg/server/options/recommended.go#L43) admission options are enabled by default . This was not the case in 1.9 release. Admission plugins seem unnecessary for a webhook server. So, I am disabling it.
    
    If this is left enabled, then RBAC permissions need to be updated accordingly.
    ```
    - apiGroups:
      - admissionregistration.k8s.io
      resources:
        - mutatingwebhookconfigurations
        - validatingwebhookconfigurations
      verbs: ["get","list"]
    ```

--- pkg/cmds/server/start.go
+++ pkg/cmds/server/start.go
@@ -32,6 +32,7 @@ func NewKubedOptions(out, errOut io.Writer) *KubedOptions {
 		StdErr:             errOut,
 	}
 	o.RecommendedOptions.Etcd = nil
+	o.RecommendedOptions.Admission = nil
 
 	return o
 }
