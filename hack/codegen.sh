45d8b46eb2d0c3fad5e1b487e80229f6069ba967
Tamal Saha <tamal@appscode.com>
2018-07-08 19:20:43 -0700

    Format shell script (#290)
    find hack | grep sh | xargs shfmt -l -w -ci -i 2 {}

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -35,13 +35,13 @@ docker run --rm -ti -u $(id -u):$(id -g) \
 
 # Generate openapi
 docker run --rm -ti -u $(id -u):$(id -g) \
-    -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
-    -w "$DOCKER_REPO_ROOT" \
-    appscode/gengo:release-1.10 openapi-gen \
-    --v 1 --logtostderr \
-    --go-header-file "hack/gengo/boilerplate.go.txt" \
-    --input-dirs "$PACKAGE_NAME/apis/kubed/v1alpha1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/version,k8s.io/api/core/v1" \
-    --output-package "$PACKAGE_NAME/apis/kubed/v1alpha1"
+  -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
+  -w "$DOCKER_REPO_ROOT" \
+  appscode/gengo:release-1.10 openapi-gen \
+  --v 1 --logtostderr \
+  --go-header-file "hack/gengo/boilerplate.go.txt" \
+  --input-dirs "$PACKAGE_NAME/apis/kubed/v1alpha1,k8s.io/apimachinery/pkg/apis/meta/v1,k8s.io/apimachinery/pkg/api/resource,k8s.io/apimachinery/pkg/runtime,k8s.io/apimachinery/pkg/version,k8s.io/api/core/v1" \
+  --output-package "$PACKAGE_NAME/apis/kubed/v1alpha1"
 
 # Generate crds.yaml and swagger.json
 go run ./hack/gencrd/main.go
--- hack/deploy/kubed.sh
+++ hack/deploy/kubed.sh
@@ -2,86 +2,91 @@
 set -eou pipefail
 
 echo "checking kubeconfig context"
-kubectl config current-context || { echo "Set a context (kubectl use-context <context>) out of the following:"; echo; kubectl config get-contexts; exit 1; }
+kubectl config current-context || {
+  echo "Set a context (kubectl use-context <context>) out of the following:"
+  echo
+  kubectl config get-contexts
+  exit 1
+}
 echo ""
 
 # http://redsymbol.net/articles/bash-exit-traps/
-function cleanup {
-	rm -rf $ONESSL ca.crt ca.key server.crt server.key
+function cleanup() {
+  rm -rf $ONESSL ca.crt ca.key server.crt server.key
 }
 
 export APPSCODE_ENV=${APPSCODE_ENV:-prod}
 if [ "$APPSCODE_ENV" != "test-concourse" ]; then
-    trap cleanup EXIT
+  trap cleanup EXIT
 fi
 
 # ref: https://github.com/appscodelabs/libbuild/blob/master/common/lib.sh#L55
 inside_git_repo() {
-    git rev-parse --is-inside-work-tree > /dev/null 2>&1
-    inside_git=$?
-    if [ "$inside_git" -ne 0 ]; then
-        echo "Not inside a git repository"
-        exit 1
-    fi
+  git rev-parse --is-inside-work-tree >/dev/null 2>&1
+  inside_git=$?
+  if [ "$inside_git" -ne 0 ]; then
+    echo "Not inside a git repository"
+    exit 1
+  fi
 }
 
 detect_tag() {
-    inside_git_repo
+  inside_git_repo
 
-    # http://stackoverflow.com/a/1404862/3476121
-    git_tag=$(git describe --exact-match --abbrev=0 2>/dev/null || echo '')
+  # http://stackoverflow.com/a/1404862/3476121
+  git_tag=$(git describe --exact-match --abbrev=0 2>/dev/null || echo '')
 
-    commit_hash=$(git rev-parse --verify HEAD)
-    git_branch=$(git rev-parse --abbrev-ref HEAD)
-    commit_timestamp=$(git show -s --format=%ct)
+  commit_hash=$(git rev-parse --verify HEAD)
+  git_branch=$(git rev-parse --abbrev-ref HEAD)
+  commit_timestamp=$(git show -s --format=%ct)
 
-    if [ "$git_tag" != '' ]; then
-        TAG=$git_tag
-        TAG_STRATEGY='git_tag'
-    elif [ "$git_branch" != 'master' ] && [ "$git_branch" != 'HEAD' ] && [[ "$git_branch" != release-* ]]; then
-        TAG=$git_branch
-        TAG_STRATEGY='git_branch'
-    else
-        hash_ver=$(git describe --tags --always --dirty)
-        TAG="${hash_ver}"
-        TAG_STRATEGY='commit_hash'
-    fi
+  if [ "$git_tag" != '' ]; then
+    TAG=$git_tag
+    TAG_STRATEGY='git_tag'
+  elif [ "$git_branch" != 'master' ] && [ "$git_branch" != 'HEAD' ] && [[ "$git_branch" != release-* ]]; then
+    TAG=$git_branch
+    TAG_STRATEGY='git_branch'
+  else
+    hash_ver=$(git describe --tags --always --dirty)
+    TAG="${hash_ver}"
+    TAG_STRATEGY='commit_hash'
+  fi
 
-    export TAG
-    export TAG_STRATEGY
-    export git_tag
-    export git_branch
-    export commit_hash
-    export commit_timestamp
+  export TAG
+  export TAG_STRATEGY
+  export git_tag
+  export git_branch
+  export commit_hash
+  export commit_timestamp
 }
 
 # https://stackoverflow.com/a/677212/244009
 if [ -x "$(command -v onessl)" ]; then
-    export ONESSL=onessl
+  export ONESSL=onessl
 else
-    # ref: https://stackoverflow.com/a/27776822/244009
-    case "$(uname -s)" in
-        Darwin)
-            curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-darwin-amd64
-            chmod +x onessl
-            export ONESSL=./onessl
-            ;;
+  # ref: https://stackoverflow.com/a/27776822/244009
+  case "$(uname -s)" in
+    Darwin)
+      curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-darwin-amd64
+      chmod +x onessl
+      export ONESSL=./onessl
+      ;;
 
-        Linux)
-            curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64
-            chmod +x onessl
-            export ONESSL=./onessl
-            ;;
+    Linux)
+      curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64
+      chmod +x onessl
+      export ONESSL=./onessl
+      ;;
 
-        CYGWIN*|MINGW32*|MSYS*)
-            curl -fsSL -o onessl.exe https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-windows-amd64.exe
-            chmod +x onessl.exe
-            export ONESSL=./onessl.exe
-            ;;
-        *)
-            echo 'other OS'
-            ;;
-    esac
+    CYGWIN* | MINGW32* | MSYS*)
+      curl -fsSL -o onessl.exe https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-windows-amd64.exe
+      chmod +x onessl.exe
+      export ONESSL=./onessl.exe
+      ;;
+    *)
+      echo 'other OS'
+      ;;
+  esac
 fi
 
 # ref: https://stackoverflow.com/a/7069755/244009
@@ -103,129 +108,132 @@ export KUBED_CONFIG_CLUSTER_NAME=unicorn
 export KUBED_CONFIG_ENABLE_APISERVER=false
 
 export SCRIPT_LOCATION="curl -fsSL https://raw.githubusercontent.com/appscode/kubed/0.7.0/"
-if [[ "$APPSCODE_ENV" = "dev" || "$APPSCODE_ENV" = "test-concourse" ]]; then
-    detect_tag
-    export SCRIPT_LOCATION="cat "
-    export KUBED_IMAGE_TAG=$TAG
-    export KUBED_IMAGE_PULL_POLICY=Always
+if [[ "$APPSCODE_ENV" == "dev" || "$APPSCODE_ENV" == "test-concourse" ]]; then
+  detect_tag
+  export SCRIPT_LOCATION="cat "
+  export KUBED_IMAGE_TAG=$TAG
+  export KUBED_IMAGE_PULL_POLICY=Always
 fi
 
 KUBE_APISERVER_VERSION=$(kubectl version -o=json | $ONESSL jsonpath '{.serverVersion.gitVersion}')
 $ONESSL semver --check='<1.9.0' $KUBE_APISERVER_VERSION || { export KUBED_CONFIG_ENABLE_APISERVER=true; }
 
 show_help() {
-    echo "kubed.sh - install Kubernetes cluster daemon"
-    echo " "
-    echo "kubed.sh [options]"
-    echo " "
-    echo "options:"
-    echo "-h, --help                         show brief help"
-    echo "-n, --namespace=NAMESPACE          specify namespace (default: kube-system)"
-    echo "    --rbac                         create RBAC roles and bindings (default: true)"
-    echo "    --docker-registry              docker registry used to pull kubed images (default: appscode)"
-    echo "    --image-pull-secret            name of secret used to pull kubed operator images"
-    echo "    --run-on-master                run kubed operator on master"
-    echo "    --cluster-name                 name of cluster (default: unicorn)"
-    echo "    --enable-apiserver             enable/disable kubed apiserver"
-    echo "    --enable-analytics             send usage events to Google Analytics (default: true)"
-    echo "    --uninstall                    uninstall kubed"
+  echo "kubed.sh - install Kubernetes cluster daemon"
+  echo " "
+  echo "kubed.sh [options]"
+  echo " "
+  echo "options:"
+  echo "-h, --help                         show brief help"
+  echo "-n, --namespace=NAMESPACE          specify namespace (default: kube-system)"
+  echo "    --rbac                         create RBAC roles and bindings (default: true)"
+  echo "    --docker-registry              docker registry used to pull kubed images (default: appscode)"
+  echo "    --image-pull-secret            name of secret used to pull kubed operator images"
+  echo "    --run-on-master                run kubed operator on master"
+  echo "    --cluster-name                 name of cluster (default: unicorn)"
+  echo "    --enable-apiserver             enable/disable kubed apiserver"
+  echo "    --enable-analytics             send usage events to Google Analytics (default: true)"
+  echo "    --uninstall                    uninstall kubed"
 }
 
 while test $# -gt 0; do
-    case "$1" in
-        -h|--help)
-            show_help
-            exit 0
-            ;;
-        -n)
-            shift
-            if test $# -gt 0; then
-                export KUBED_NAMESPACE=$1
-            else
-                echo "no namespace specified"
-                exit 1
-            fi
-            shift
-            ;;
-        --namespace*)
-            export KUBED_NAMESPACE=`echo $1 | sed -e 's/^[^=]*=//g'`
-            shift
-            ;;
-        --docker-registry*)
-            export KUBED_DOCKER_REGISTRY=`echo $1 | sed -e 's/^[^=]*=//g'`
-            shift
-            ;;
-        --image-pull-secret*)
-            secret=`echo $1 | sed -e 's/^[^=]*=//g'`
-            export KUBED_IMAGE_PULL_SECRET="name: '$secret'"
-            shift
-            ;;
-        --enable-analytics*)
-            val=`echo $1 | sed -e 's/^[^=]*=//g'`
-            if [ "$val" = "false" ]; then
-                export SEARCHLIGHT_ENABLE_ANALYTICS=false
-            fi
-            shift
-            ;;
-        --rbac*)
-            val=`echo $1 | sed -e 's/^[^=]*=//g'`
-            if [ "$val" = "false" ]; then
-                export KUBED_SERVICE_ACCOUNT=default
-                export KUBED_ENABLE_RBAC=false
-            fi
-            shift
-            ;;
-        --run-on-master)
-            export KUBED_RUN_ON_MASTER=1
-            shift
-            ;;
-        --cluster-name*)
-            export KUBED_CONFIG_CLUSTER_NAME=`echo $1 | sed -e 's/^[^=]*=//g'`
-            shift
-            ;;
-        --enable-apiserver*)
-            val=`echo $1 | sed -e 's/^[^=]*=//g'`
-            if [ "$val" = "false" ]; then
-                export KUBED_CONFIG_ENABLE_APISERVER=false
-            else
-                export KUBED_CONFIG_ENABLE_APISERVER=true
-            fi
-            shift
-            ;;
-        --uninstall)
-            export KUBED_UNINSTALL=1
-            shift
-            ;;
-        *)
-            show_help
-            exit 1
-            ;;
-    esac
+  case "$1" in
+    -h | --help)
+      show_help
+      exit 0
+      ;;
+    -n)
+      shift
+      if test $# -gt 0; then
+        export KUBED_NAMESPACE=$1
+      else
+        echo "no namespace specified"
+        exit 1
+      fi
+      shift
+      ;;
+    --namespace*)
+      export KUBED_NAMESPACE=$(echo $1 | sed -e 's/^[^=]*=//g')
+      shift
+      ;;
+    --docker-registry*)
+      export KUBED_DOCKER_REGISTRY=$(echo $1 | sed -e 's/^[^=]*=//g')
+      shift
+      ;;
+    --image-pull-secret*)
+      secret=$(echo $1 | sed -e 's/^[^=]*=//g')
+      export KUBED_IMAGE_PULL_SECRET="name: '$secret'"
+      shift
+      ;;
+    --enable-analytics*)
+      val=$(echo $1 | sed -e 's/^[^=]*=//g')
+      if [ "$val" = "false" ]; then
+        export SEARCHLIGHT_ENABLE_ANALYTICS=false
+      fi
+      shift
+      ;;
+    --rbac*)
+      val=$(echo $1 | sed -e 's/^[^=]*=//g')
+      if [ "$val" = "false" ]; then
+        export KUBED_SERVICE_ACCOUNT=default
+        export KUBED_ENABLE_RBAC=false
+      fi
+      shift
+      ;;
+    --run-on-master)
+      export KUBED_RUN_ON_MASTER=1
+      shift
+      ;;
+    --cluster-name*)
+      export KUBED_CONFIG_CLUSTER_NAME=$(echo $1 | sed -e 's/^[^=]*=//g')
+      shift
+      ;;
+    --enable-apiserver*)
+      val=$(echo $1 | sed -e 's/^[^=]*=//g')
+      if [ "$val" = "false" ]; then
+        export KUBED_CONFIG_ENABLE_APISERVER=false
+      else
+        export KUBED_CONFIG_ENABLE_APISERVER=true
+      fi
+      shift
+      ;;
+    --uninstall)
+      export KUBED_UNINSTALL=1
+      shift
+      ;;
+    *)
+      show_help
+      exit 1
+      ;;
+  esac
 done
 
 if [ "$KUBED_UNINSTALL" -eq 1 ]; then
-    # delete webhooks and apiservices
-    kubectl delete validatingwebhookconfiguration -l app=kubed || true
-    kubectl delete mutatingwebhookconfiguration -l app=kubed || true
-    kubectl delete apiservice -l app=kubed
-    # delete kubed operator
-    kubectl delete deployment -l app=kubed --namespace $KUBED_NAMESPACE
-    kubectl delete service -l app=kubed --namespace $KUBED_NAMESPACE
-    kubectl delete secret -l app=kubed --namespace $KUBED_NAMESPACE
-    # delete RBAC objects, if --rbac flag was used.
-    kubectl delete serviceaccount -l app=kubed --namespace $KUBED_NAMESPACE
-    kubectl delete clusterrolebindings -l app=kubed
-    kubectl delete clusterrole -l app=kubed
-    kubectl delete rolebindings -l app=kubed --namespace $KUBED_NAMESPACE
-    kubectl delete role -l app=kubed --namespace $KUBED_NAMESPACE
-    # delete user roles
-    kubectl delete clusterrole appscode:kubed:view
+  # delete webhooks and apiservices
+  kubectl delete validatingwebhookconfiguration -l app=kubed || true
+  kubectl delete mutatingwebhookconfiguration -l app=kubed || true
+  kubectl delete apiservice -l app=kubed
+  # delete kubed operator
+  kubectl delete deployment -l app=kubed --namespace $KUBED_NAMESPACE
+  kubectl delete service -l app=kubed --namespace $KUBED_NAMESPACE
+  kubectl delete secret -l app=kubed --namespace $KUBED_NAMESPACE
+  # delete RBAC objects, if --rbac flag was used.
+  kubectl delete serviceaccount -l app=kubed --namespace $KUBED_NAMESPACE
+  kubectl delete clusterrolebindings -l app=kubed
+  kubectl delete clusterrole -l app=kubed
+  kubectl delete rolebindings -l app=kubed --namespace $KUBED_NAMESPACE
+  kubectl delete role -l app=kubed --namespace $KUBED_NAMESPACE
+  # delete user roles
+  kubectl delete clusterrole appscode:kubed:view
 
-    exit 0
+  exit 0
 fi
 
 echo "checking whether extended apiserver feature is enabled"
-$ONESSL has-keys configmap --namespace=kube-system --keys=requestheader-client-ca-file extension-apiserver-authentication || { echo "Set --requestheader-client-ca-file flag on Kubernetes apiserver"; exit 1; }
+$ONESSL has-keys configmap --namespace=kube-system --keys=requestheader-client-ca-file extension-apiserver-authentication || {
+  echo "Set --requestheader-client-ca-file flag on Kubernetes apiserver"
+  exit 1
+}
 echo ""
 
 env | sort | grep KUBED*
@@ -241,38 +249,44 @@ export TLS_SERVING_CERT=$(cat server.crt | $ONESSL base64)
 export TLS_SERVING_KEY=$(cat server.key | $ONESSL base64)
 
 CONFIG_FOUND=1
-kubectl get secret kubed-config -n $KUBED_NAMESPACE > /dev/null 2>&1 || CONFIG_FOUND=0
+kubectl get secret kubed-config -n $KUBED_NAMESPACE >/dev/null 2>&1 || CONFIG_FOUND=0
 if [ $CONFIG_FOUND -eq 0 ]; then
-    config=`${SCRIPT_LOCATION}hack/deploy/config.yaml | $ONESSL envsubst`
-    kubectl create secret generic kubed-config -n $KUBED_NAMESPACE \
-        --from-literal=config.yaml="${config}"
+  config=$(${SCRIPT_LOCATION}hack/deploy/config.yaml | $ONESSL envsubst)
+  kubectl create secret generic kubed-config -n $KUBED_NAMESPACE \
+    --from-literal=config.yaml="${config}"
 fi
 kubectl label secret kubed-config app=kubed -n $KUBED_NAMESPACE --overwrite
 
 ${SCRIPT_LOCATION}hack/deploy/operator.yaml | $ONESSL envsubst | kubectl apply -f -
 
 if [ "$KUBED_ENABLE_RBAC" = true ]; then
-    kubectl create serviceaccount $KUBED_SERVICE_ACCOUNT --namespace $KUBED_NAMESPACE
-    kubectl label serviceaccount $KUBED_SERVICE_ACCOUNT app=kubed --namespace $KUBED_NAMESPACE
-    ${SCRIPT_LOCATION}hack/deploy/rbac-list.yaml | $ONESSL envsubst | kubectl auth reconcile -f -
-    ${SCRIPT_LOCATION}hack/deploy/user-roles.yaml | $ONESSL envsubst | kubectl auth reconcile -f -
+  kubectl create serviceaccount $KUBED_SERVICE_ACCOUNT --namespace $KUBED_NAMESPACE
+  kubectl label serviceaccount $KUBED_SERVICE_ACCOUNT app=kubed --namespace $KUBED_NAMESPACE
+  ${SCRIPT_LOCATION}hack/deploy/rbac-list.yaml | $ONESSL envsubst | kubectl auth reconcile -f -
+  ${SCRIPT_LOCATION}hack/deploy/user-roles.yaml | $ONESSL envsubst | kubectl auth reconcile -f -
 fi
 
 if [ "$KUBED_RUN_ON_MASTER" -eq 1 ]; then
-    kubectl patch deploy kubed-operator -n $KUBED_NAMESPACE \
-      --patch="$(${SCRIPT_LOCATION}hack/deploy/run-on-master.yaml)"
+  kubectl patch deploy kubed-operator -n $KUBED_NAMESPACE \
+    --patch="$(${SCRIPT_LOCATION}hack/deploy/run-on-master.yaml)"
 fi
 
 if [ "$KUBED_CONFIG_ENABLE_APISERVER" = true ]; then
-    ${SCRIPT_LOCATION}hack/deploy/apiservices.yaml | $ONESSL envsubst | kubectl apply -f -
+  ${SCRIPT_LOCATION}hack/deploy/apiservices.yaml | $ONESSL envsubst | kubectl apply -f -
 fi
 
 echo "waiting until kubed deployment is ready"
-$ONESSL wait-until-ready deployment kubed-operator --namespace $KUBED_NAMESPACE || { echo "Kubed deployment failed to be ready"; exit 1; }
+$ONESSL wait-until-ready deployment kubed-operator --namespace $KUBED_NAMESPACE || {
+  echo "Kubed deployment failed to be ready"
+  exit 1
+}
 
 if [ "$KUBED_CONFIG_ENABLE_APISERVER" = true ]; then
-    echo "waiting until kubed apiservice is available"
-    $ONESSL wait-until-ready apiservice v1alpha1.kubed.appscode.com || { echo "Kubed apiservice failed to be ready"; exit 1; }
+  echo "waiting until kubed apiservice is available"
+  $ONESSL wait-until-ready apiservice v1alpha1.kubed.appscode.com || {
+    echo "Kubed apiservice failed to be ready"
+    exit 1
+  }
 fi
 
 echo
