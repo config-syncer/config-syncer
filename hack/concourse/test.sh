aedd0131e63b5bf0718d7e1190eac89eefd4f347
Tamal Saha <tamal@appscode.com>
2018-07-08 19:20:19 -0700

    Format shell script
    find hack | grep sh | xargs shfmt -l -w -ci -i 2 {}

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -8,18 +8,18 @@ docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker run hello-world
 
 # install python pip
-apt-get update > /dev/null
-apt-get install -y python python-pip > /dev/null
+apt-get update >/dev/null
+apt-get install -y python python-pip >/dev/null
 
 # install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &> /dev/null
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &>/dev/null
 chmod +x ./kubectl
 mv ./kubectl /bin/kubectl
 
 # install onessl
-curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 \
-  && chmod +x onessl \
-  && mv onessl /usr/local/bin/
+curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.3.0/onessl-linux-amd64 &&
+  chmod +x onessl &&
+  mv onessl /usr/local/bin/
 
 # install pharmer
 pushd /tmp
@@ -28,21 +28,21 @@ chmod +x pharmer-linux-amd64
 mv pharmer-linux-amd64 /bin/pharmer
 popd
 
-function cleanup_test_stuff {
-    rm -rf $ONESSL ca.crt ca.key server.crt server.key || true
+function cleanup_test_stuff() {
+  rm -rf $ONESSL ca.crt ca.key server.crt server.key || true
 
-    # delete cluster on exit
-    pharmer get cluster || true
-    pharmer delete cluster $NAME || true
-    pharmer get cluster || true
-    sleep 300 || true
-    pharmer apply $NAME || true
-    pharmer get cluster || true
+  # delete cluster on exit
+  pharmer get cluster || true
+  pharmer delete cluster $NAME || true
+  pharmer get cluster || true
+  sleep 300 || true
+  pharmer apply $NAME || true
+  pharmer get cluster || true
 
-    # delete docker image on exit
-    curl -LO https://raw.githubusercontent.com/appscodelabs/libbuild/master/docker.py || true
-    chmod +x docker.py || true
-    ./docker.py del_tag appscodeci kubed $KUBED_IMAGE_TAG || true
+  # delete docker image on exit
+  curl -LO https://raw.githubusercontent.com/appscodelabs/libbuild/master/docker.py || true
+  chmod +x docker.py || true
+  ./docker.py del_tag appscodeci kubed $KUBED_IMAGE_TAG || true
 }
 trap cleanup_test_stuff EXIT
 
@@ -63,7 +63,7 @@ export DOCKER_REGISTRY=appscodeci
 popd
 
 #create credential file for pharmer
-cat > cred.json <<EOF
+cat >cred.json <<EOF
 {
     "token" : "$TOKEN"
 }
@@ -80,7 +80,7 @@ sleep 300
 kubectl get nodes
 
 # create storageclass
-cat > sc.yaml <<EOF
+cat >sc.yaml <<EOF
 apiVersion: storage.k8s.io/v1
 kind: StorageClass
 metadata:
--- hack/docker/setup.sh
+++ hack/docker/setup.sh
@@ -20,38 +20,38 @@ OSM_VER=${OSM_VER:-0.7.0}
 DIST=$REPO_ROOT/dist
 mkdir -p $DIST
 if [ -f "$DIST/.tag" ]; then
-    export $(cat $DIST/.tag | xargs)
+  export $(cat $DIST/.tag | xargs)
 fi
 
 clean() {
-    pushd $REPO_ROOT/hack/docker
-    rm -rf kubed
-    popd
+  pushd $REPO_ROOT/hack/docker
+  rm -rf kubed
+  popd
 }
 
 build_binary() {
-    pushd $REPO_ROOT
-    ./hack/builddeps.sh
-    ./hack/make.py build kubed
-    detect_tag $REPO_ROOT/dist/.tag
+  pushd $REPO_ROOT
+  ./hack/builddeps.sh
+  ./hack/make.py build kubed
+  detect_tag $REPO_ROOT/dist/.tag
 
-    # Download restic
-    rm -rf $DIST/osm
-    mkdir $DIST/osm
-    cd $DIST/osm
-    wget https://cdn.appscode.com/binaries/osm/${OSM_VER}/osm-alpine-amd64
-    chmod +x osm-alpine-amd64
-    popd
+  # Download restic
+  rm -rf $DIST/osm
+  mkdir $DIST/osm
+  cd $DIST/osm
+  wget https://cdn.appscode.com/binaries/osm/${OSM_VER}/osm-alpine-amd64
+  chmod +x osm-alpine-amd64
+  popd
 }
 
 build_docker() {
-    pushd $REPO_ROOT/hack/docker
-    cp $DIST/kubed/kubed-alpine-amd64 kubed
-    chmod 755 kubed
-    cp $DIST/osm/osm-alpine-amd64 osm
-    chmod 755 osm
+  pushd $REPO_ROOT/hack/docker
+  cp $DIST/kubed/kubed-alpine-amd64 kubed
+  chmod 755 kubed
+  cp $DIST/osm/osm-alpine-amd64 osm
+  chmod 755 osm
 
-    cat >Dockerfile <<EOL
+  cat >Dockerfile <<EOL
 FROM alpine
 
 RUN set -x \
@@ -61,41 +61,40 @@ COPY osm /usr/bin/osm
 COPY kubed /usr/bin/kubed
 ENTRYPOINT ["kubed"]
 EOL
-    local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
-    echo $cmd; $cmd
+  local cmd="docker build -t $DOCKER_REGISTRY/$IMG:$TAG ."
+  echo $cmd; $cmd
 
-    rm kubed osm Dockerfile
-    popd
+  rm kubed osm Dockerfile
+  popd
 }
 
 build() {
-    build_binary
-    build_docker
+  build_binary
+  build_docker
 }
 
-
 docker_push() {
-    if [ "$APPSCODE_ENV" = "prod" ]; then
-        echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
-        exit 1
-    fi
-    if [ "$TAG_STRATEGY" = "git_tag" ]; then
-        echo "Are you trying to 'release' binaries to prod?"
-        exit 1
-    fi
-    hub_canary
+  if [ "$APPSCODE_ENV" = "prod" ]; then
+    echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
+    exit 1
+  fi
+  if [ "$TAG_STRATEGY" = "git_tag" ]; then
+    echo "Are you trying to 'release' binaries to prod?"
+    exit 1
+  fi
+  hub_canary
 }
 
 docker_release() {
-    if [ "$APPSCODE_ENV" != "prod" ]; then
-        echo "'release' only works in PROD env."
-        exit 1
-    fi
-    if [ "$TAG_STRATEGY" != "git_tag" ]; then
-        echo "'apply_tag' to release binaries and/or docker images."
-        exit 1
-    fi
-    hub_up
+  if [ "$APPSCODE_ENV" != "prod" ]; then
+    echo "'release' only works in PROD env."
+    exit 1
+  fi
+  if [ "$TAG_STRATEGY" != "git_tag" ]; then
+    echo "'apply_tag' to release binaries and/or docker images."
+    exit 1
+  fi
+  hub_up
 }
 
 source_repo $@
