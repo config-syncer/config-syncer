72f3b51e6561474ee48097697a833416d55f5b94
Arnob kumar saha <arnob@appscode.com>
2026-09-23 17:40:18 +0600

    Fix release checkout conflicting tag fetch
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -41,7 +41,8 @@ func init() {
 
 	// TODO: keep the generic API server from wanting this
 	unversioned := schema.GroupVersion{Group: "", Version: "v1"}
-	Scheme.AddUnversionedTypes(unversioned,
+	Scheme.AddUnversionedTypes(
+		unversioned,
 		&metav1.Status{},
 		&metav1.APIVersions{},
 		&metav1.APIGroupList{},
